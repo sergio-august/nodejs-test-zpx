c1ef46f02bb206046cd7ff560ccd29c61f8a755b
mrgromov <mailrxtx@gmail.com>
2019-04-16 23:20:21 +0400

    Calculating rewritten using Promise

---
+++ server/CalculateStatistics.ts
@@ -0,0 +1,136 @@
+import { IncomingMessage } from "http"
+import { IConfig } from "./config"
+
+interface ITransaction {
+	time: number
+	amount: number
+	sender: string
+	receiver: string
+}
+
+interface IStat {
+	total: {
+		amount: number
+		transactions: number
+	}
+	customerStatistics: {
+		[name: string]: {
+			transactions: number
+			totalSent: number
+			totalReceived: number
+		}
+	}
+	dayStatistics: {
+		[timestamp: string]: {
+			transactions: number
+		}
+	}
+}
+
+function decodeFrame(chunk: Buffer, config: IConfig): ITransaction {
+	function trimNullRight(str: string): string {
+		while (str[str.length - 1] === "\0") {
+			str = str.slice(0, str.length - 1)
+		}
+		return str
+	}
+
+	if (chunk.length !== config.frameSize) {
+		throw new Error(
+			"Failed to parse frame: frame size is incorrect: " +
+				`expected ${config.frameSize}, but got ${chunk.length}`
+		)
+	}
+	const { offset } = config
+	try {
+		const sender = trimNullRight(chunk.toString("utf8", offset.sender, offset.sender + 32).trimRight())
+		const receiver = trimNullRight(chunk.toString("utf8", offset.receiver, offset.receiver + 32).trimRight())
+		const amount = chunk.readIntBE(offset.amount, 4)
+		const time = chunk.readIntBE(offset.timestamp, 6)
+		return { time, sender, receiver, amount }
+	} catch (error) {
+		throw new Error("Failed to parse frame: offset problem!\n" + error.message)
+	}
+}
+
+
+function processTransaction(stat: IStat, data: ITransaction) {
+	const { time, sender, receiver, amount } = data
+	// console.log(`[${time}] ${sender} -> ${receiver} (${amount})`)
+	stat.total.transactions++
+	stat.total.amount += amount
+	// Per day stats
+	const day = new Date(time)
+	day.setHours(0)
+	day.setMinutes(0)
+	day.setSeconds(0)
+	day.setMilliseconds(0)
+	if (stat.customerStatistics)
+		if (stat.dayStatistics[day.getTime()] === undefined) {
+			stat.dayStatistics[day.getTime()] = { transactions: 1 }
+		} else {
+			stat.dayStatistics[day.getTime()].transactions++
+		}
+	// Per user stats
+	if (stat.customerStatistics[sender] === undefined) {
+		stat.customerStatistics[sender] = {
+			transactions: 1,
+			totalSent: amount,
+			totalReceived: 0
+		}
+	} else {
+		stat.customerStatistics[sender].transactions++
+		stat.customerStatistics[sender].totalSent += amount
+	}
+
+	if (stat.customerStatistics[receiver] === undefined) {
+		stat.customerStatistics[receiver] = {
+			transactions: 1,
+			totalSent: 0,
+			totalReceived: amount
+		}
+	} else {
+		stat.customerStatistics[receiver].transactions++
+		stat.customerStatistics[receiver].totalReceived += amount
+	}
+}
+
+export default function CalculateStatistics(config: IConfig) {
+
+	return function(request: IncomingMessage): Promise<IStat> {
+
+		const stat: IStat = {
+			dayStatistics: {},
+			customerStatistics: {},
+			total: {
+				amount: 0,
+				transactions: 0
+			}
+		}
+
+		return new Promise((resolve, reject) => {
+			request
+				.on("readable", () => {
+					let chunk
+					while (null !== (chunk = request.read(74))) {
+						try {
+							const transaction = decodeFrame(chunk, config)
+							processTransaction(stat, transaction)
+						} catch (error) {
+							reject(error)
+						}
+					}
+				})
+				.on("end", () => {
+					resolve(stat)
+				})
+				.on("error", (error: Error) => {
+					reject(error)
+				})
+				.on("close", () => {
+					reject(new Error("Client request closed"))
+				})
+		})
+	}
+
+}
--- server/server.ts
+++ server/server.ts
@@ -1,149 +1,8 @@
 import { createServer, IncomingMessage, ServerResponse } from "http"
-import { Transform } from "stream"
+import CalculateStatistics from "./CalculateStatistics"
+import config from "./config"
 
-const port = 3000
-
-interface ITransaction {
-	time: number
-	amount: number
-	sender: string
-	receiver: string
-}
-
-interface IStat {
-	total: {
-		amount: number
-		transactions: number
-	}
-	customerStatistics: {
-		[name: string]: {
-			transactions: number
-			totalSent: number
-			totalReceived: number
-		}
-	}
-	dayStatistics: {
-		[timestamp: string]: {
-			transactions: number
-		}
-	}
-}
-
-interface IFrameConfig {
-	frameSize: number
-	offset: {
-		sender: number // Sender name (string, 32 bytes)
-		receiver: number // Receiver name (string, 32 bytes)
-		amount: number // Amount in cents (4 bytes)
-		timestamp: number // Date as timestamp in ms (6 bytes)
-	}
-}
-
-const frameConfig: IFrameConfig = {
-	frameSize: 74,
-	offset: {
-		sender: 0,
-		receiver: 32,
-		amount: 64,
-		timestamp: 68
-	}
-}
-
-function decodeFrame(chunk: Buffer, frameConfig: IFrameConfig): ITransaction {
-	function trimNullRight(str: string): string {
-		while (str[str.length - 1] === "\0") {
-			str = str.slice(0, str.length - 1)
-		}
-		return str
-	}
-
-	if (chunk.length !== frameConfig.frameSize) {
-		throw new Error(
-			"Failed to parse frame: frame size is incorrect: " +
-				`expected ${frameConfig.frameSize}, but got ${chunk.length}`
-		)
-	}
-	const { offset } = frameConfig
-	try {
-		const sender = trimNullRight(chunk.toString("utf8", offset.sender, offset.sender + 32).trimRight())
-		const receiver = trimNullRight(chunk.toString("utf8", offset.receiver, offset.receiver + 32).trimRight())
-		const amount = chunk.readIntBE(offset.amount, 4)
-		const time = chunk.readIntBE(offset.timestamp, 6)
-		return { time, sender, receiver, amount }
-	} catch (error) {
-		throw new Error("Failed to parse frame: offset problem!\n" + error.message)
-	}
-}
-
-function calculateStat(stat: IStat, data: ITransaction) {
-	const { time, sender, receiver, amount } = data
-	// console.log(`[${time}] ${sender} -> ${receiver} (${amount})`)
-	stat.total.transactions++
-	stat.total.amount += amount
-	// Per day stats
-	const day = new Date(time)
-	day.setHours(0)
-	day.setMinutes(0)
-	day.setSeconds(0)
-	day.setMilliseconds(0)
-	if (stat.customerStatistics)
-		if (stat.dayStatistics[day.getTime()] === undefined) {
-			stat.dayStatistics[day.getTime()] = { transactions: 1 }
-		} else {
-			stat.dayStatistics[day.getTime()].transactions++
-		}
-	// Per user stats
-	if (stat.customerStatistics[sender] === undefined) {
-		stat.customerStatistics[sender] = {
-			transactions: 1,
-			totalSent: amount,
-			totalReceived: 0
-		}
-	} else {
-		stat.customerStatistics[sender].transactions++
-		stat.customerStatistics[sender].totalSent += amount
-	}
-
-	if (stat.customerStatistics[receiver] === undefined) {
-		stat.customerStatistics[receiver] = {
-			transactions: 1,
-			totalSent: 0,
-			totalReceived: amount
-		}
-	} else {
-		stat.customerStatistics[receiver].transactions++
-		stat.customerStatistics[receiver].totalReceived += amount
-	}
-}
-
-class CalculateStatStream extends Transform {
-	stat: IStat
-	constructor(options: any) {
-		super(options)
-		this.stat = {
-			total: {
-				amount: 0,
-				transactions: 0
-			},
-			customerStatistics: {},
-			dayStatistics: {}
-		}
-	}
-	_transform(chunk: Buffer, enc: string, next: any) {
-		try {
-			const transaction = decodeFrame(chunk, frameConfig)
-			calculateStat(this.stat, transaction)
-			next()
-		} catch (error) {
-			next(error)
-		}
-	}
-	_flush(next: any) {
-		console.log(this.stat.total)
-		this.push(JSON.stringify(this.stat, null, "\t"))
-		next()
-	}
-}
+const calculate = CalculateStatistics(config)
 
 const server = createServer((request: IncomingMessage, response: ServerResponse) => {
 	switch (request.url) {
@@ -158,27 +17,16 @@ const server = createServer((request: IncomingMessage, response: ServerResponse)
 				response.end(JSON.stringify({ error: "'content-type' should be 'application/octet-stream'" }))
 			}
 
-			const calcStream = new CalculateStatStream({ writableObjectMode: true })
-
-			calcStream
-				.on("error", error => {
+			calculate(request)
+				.then(stat => {
+					console.log(stat.total)
+					response.end(JSON.stringify(stat, null, "\t"))
+				})
+				.catch(error => {
 					console.error(error)
 					response.statusCode = 415
 					response.end(JSON.stringify({ error: "Parse error", message: error.message }))
 				})
-
-			request
-				.on("error", error => {
-					console.error(error)
-					response.statusCode = 415
-					response.end(JSON.stringify({ error: "Parse error", message: error.message }))
-				})
-				.on("close", () => {
-					console.log("Client request closed")
-					calcStream.end()
-				})
-
-			request.pipe(calcStream).pipe(response)
 
 			break
 		}
@@ -197,6 +45,6 @@ server.on("clientError", (error, socket) => {
 	}
 })
 
-server.listen(port, () => {
-	console.log(`Server listening on port ${port}`)
+server.listen(config.serverPort, () => {
+	console.log(`Server is listening on port ${config.serverPort}`)
 })
